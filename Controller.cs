9651f257261ad20858ea90e3995e885f9acf9429
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-09-13 17:42:09 -0500

    InitialCommit

---
+++ Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyP2_ExamenGrupal1
+{
+    public class Controller
+    {
+        public void Execute()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PyP2_ExamenGrupal1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.Execute();
+        }
+    }
+}
